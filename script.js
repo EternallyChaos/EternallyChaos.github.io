d8b4cce4f4f649095283b6c9a718a88675419d63
EternallyChaos <100826527+EternallyChaos@users.noreply.github.com>
2023-03-01 20:59:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,456 @@
+// Char Database
+let database = {
+
+    // Pyro Chars
+
+    "yoimiya": {
+        charName: "Yoimiya",
+
+        image: "images/Character_Yoimiya.png",
+        icon: "images/Element_Pyro.png",
+        header: "char-pyro"
+    },
+    "amber": {
+        charName: "Amber1",
+        image: "images/Character_Amber.png",
+        icon: "images/Element_Pyro.png",
+        header: "char-pyro"
+    },
+    "hutao": {
+        charName: "Hu Tao",
+        image: "images/Character_Hutao.png",
+        icon: "images/Element_Pyro.png",
+        header: "char-pyro"
+    },
+    "diluc": {
+        charName: "Diluc",
+        image: "images/Character_Diluc.png",
+        icon: "images/Element_Pyro.png",
+        header: "char-pyro"
+    },
+    "amber": {
+        charName: "Amber",
+        image: "images/Character_Amber.png",
+        icon: "images/Element_Pyro.png",
+        header: "char-pyro"
+    },
+    "bennett": {
+        charName: "Bennett",
+        image: "images/Character_Bennett.png",
+        icon: "images/Element_Pyro.png",
+        header: "char-pyro"
+    },
+    "klee": {
+        charName: "Klee",
+        image: "images/Character_Klee.png",
+        icon: "images/Element_Pyro.png",
+        header: "char-pyro"
+    },
+    "xinyan": {
+        charName: "Xinyan",
+        image: "images/Character_Xinyan.png",
+        icon: "images/Element_Pyro.png",
+        header: "char-pyro"
+    },
+    "thoma": {
+        charName: "Thoma",
+        image: "images/Character_Thoma.png",
+        icon: "images/Element_Pyro.png",
+        header: "char-pyro"
+    },
+    "xiangling": {
+        charName: "Xiangling",
+        image: "images/Character_Xiangling.png",
+        icon: "images/Element_Pyro.png",
+        header: "char-pyro"
+    },
+    "yanfei": {
+        charName: "Yanfei",
+        image: "images/Character_Yanfei.png",
+        icon: "images/Element_Pyro.png",
+        header: "char-pyro"
+    },
+
+    // Hydro Char
+
+    "yelan": {
+        charName: "Yelan",
+        image: "images/Character_Yelan.png",
+        icon: "images/Element_Hydro.png",
+        header: "char-hydro"
+    },
+    "ayato": {
+        charName: "Ayato",
+        image: "images/Character_Ayato.png",
+        icon: "images/Element_Hydro.png",
+        header: "char-hydro"
+    },
+    "kokomi": {
+        charName: "Kokomi",
+        image: "images/Character_Kokomi.png",
+        icon: "images/Element_Hydro.png",
+        header: "char-hydro"
+    },
+    "mona": {
+        charName: "Mona",
+        image: "images/Character_Mona.png",
+        icon: "images/Element_Hydro.png",
+        header: "char-hydro"
+    },
+    "tartaglia": {
+        charName: "Tartaglia",
+        image: "images/Character_Tartaglia.png",
+        icon: "images/Element_Hydro.png",
+        header: "char-hydro"
+    },
+    "barbara": {
+        charName: "Barbara",
+        image: "images/Character_Barbara.png",
+        icon: "images/Element_Hydro.png",
+        header: "char-hydro"
+    },
+    "xingqiu": {
+        charName: "Xingqiu",
+        image: "images/Character_Xingqiu.png",
+        icon: "images/Element_Hydro.png",
+        header: "char-hydro"
+    },
+
+    // Electro Chars
+
+    "shogun": {
+        charName: "Raiden Shogun",
+        image: "images/Character_Shogun.png",
+        icon: "images/Element_Electro.png",
+        header: "char-electro"
+    },
+    "yae": {
+        charName: "Yae Miko",
+        image: "images/Character_Yae.png",
+        icon: "images/Element_Electro.png",
+        header: "char-electro"
+    },
+    "keqing": {
+        charName: "Keqing",
+        image: "images/Character_Keqing.png",
+        icon: "images/Element_Electro.png",
+        header: "char-electro"
+    },
+    "beidou": {
+        charName: "Beidou",
+        image: "images/Character_Beidou.png",
+        icon: "images/Element_Electro.png",
+        header: "char-electro"
+    },
+    "fischl": {
+        charName: "Fischl",
+        image: "images/Character_Fischl.png",
+        icon: "images/Element_Electro.png",
+        header: "char-electro"
+    },
+    "sara": {
+        charName: "Kujou Sara",
+        image: "images/Character_Sara.png",
+        icon: "images/Element_Electro.png",
+        header: "char-electro"
+    },
+    "shinobu": {
+        charName: "Kuki Shinobu",
+        image: "images/Character_Shinobu.png",
+        icon: "images/Element_Electro.png",
+        header: "char-electro"
+    },
+    "lisa": {
+        charName: "Lisa",
+        image: "images/Character_Lisa.png",
+        icon: "images/Element_Electro.png",
+        header: "char-electro"
+    },
+    "razor": {
+        charName: "Razor",
+        image: "images/Character_Razor.png",
+        icon: "images/Element_Electro.png",
+        header: "char-electro"
+    },
+
+    // Anemo Chars
+    "venti": {
+        charName: "Venti",
+        image: "images/Character_Venti.png",
+        icon: "images/Element_Anemo.png",
+        header: "char-anemo"
+    },
+    "jean": {
+        charName: "Jean",
+        image: "images/Character_Jean.png",
+        icon: "images/Element_Anemo.png",
+        header: "char-anemo"
+    },
+    "kazuha": {
+        charName: "Kazuha",
+        image: "images/Character_Kazuha.png",
+        icon: "images/Element_Anemo.png",
+        header: "char-anemo"
+    },
+    "xiao": {
+        charName: "Xiao",
+        image: "images/Character_Xiao.png",
+        icon: "images/Element_Anemo.png",
+        header: "char-anemo"
+    },
+    "heizou": {
+        charName: "Heizou",
+        image: "images/Character_Heizou.png",
+        icon: "images/Element_Anemo.png",
+        header: "char-anemo"
+    },
+    "sayu": {
+        charName: "Sayu",
+        image: "images/Character_Sayu.png",
+        icon: "images/Element_Anemo.png",
+        header: "char-anemo"
+    },
+    "sucrose": {
+        charName: "Sucrose",
+        image: "images/Character_Sucrose.png",
+        icon: "images/Element_Anemo.png",
+        header: "char-anemo"
+    },
+
+    // Cryo Chars
+
+    "ayaka": {
+        charName: "Ayaka",
+        image: "images/Character_Ayaka.png",
+        icon: "images/Element_Cryo.png",
+        header: "char-cryo"
+    },
+    "ganyu": {
+        charName: "Ganyu",
+        image: "images/Character_Ganyu.png",
+        icon: "images/Element_Cryo.png",
+        header: "char-cryo"
+    },
+    "eula": {
+        charName: "Eula",
+        image: "images/Character_Eula.png",
+        icon: "images/Element_Cryo.png",
+        header: "char-cryo"
+    },
+    "shenhe": {
+        charName: "Shenhe",
+        image: "images/Character_Shenhe.png",
+        icon: "images/Element_Cryo.png",
+        header: "char-cryo"
+    },
+    "qiqi": {
+        charName: "Qiqi",
+        image: "images/Character_Qiqi.png",
+        icon: "images/Element_Cryo.png",
+        header: "char-cryo"
+    },
+    "aloy": {
+        charName: "Aloy",
+        image: "images/Character_Aloy.png",
+        icon: "images/Element_Cryo.png",
+        header: "char-cryo"
+    },
+    "rosaria": {
+        charName: "Rosaria",
+        image: "images/Character_Rosaria.png",
+        icon: "images/Element_Cryo.png",
+        header: "char-cryo"
+    },
+    "diona": {
+        charName: "Diona",
+        image: "images/Character_Diona.png",
+        icon: "images/Element_Cryo.png",
+        header: "char-cryo"
+    },
+    "kaeya": {
+        charName: "Kaeya",
+        image: "images/Character_Kaeya.png",
+        icon: "images/Element_Cryo.png",
+        header: "char-cryo"
+    },
+    "chongyun": {
+        charName: "Chongyun",
+        image: "images/Character_Chongyun.png",
+        icon: "images/Element_Cryo.png",
+        header: "char-cryo"
+    },
+
+    // Geo chars
+
+    "zhongli": {
+        charName: "Zhongli",
+        image: "images/Character_Zhongli.png",
+        icon: "images/Element_Geo.png",
+        header: "char-geo"
+    },
+    "albedo": {
+        charName: "Albedo",
+        image: "images/Character_Albedo.png",
+        icon: "images/Element_Geo.png",
+        header: "char-geo"
+    },
+    "itto": {
+        charName: "Itto",
+        image: "images/Character_Itto.png",
+        icon: "images/Element_Geo.png",
+        header: "char-geo"
+    },
+    "ningguang": {
+        charName: "Ningguang",
+        image: "images/Character_Ningguang.png",
+        icon: "images/Element_Geo.png",
+        header: "char-geo"
+    },
+    "noelle": {
+        charName: "Noelle",
+        image: "images/Character_Noelle.png",
+        icon: "images/Element_Geo.png",
+        header: "char-geo"
+    },
+    "gorou": {
+        charName: "Gorou",
+        image: "images/Character_Gorou.png",
+        icon: "images/Element_Geo.png",
+        header: "char-geo"
+    },
+
+    "yunjin": {
+        charName: "Yun Jin",
+        image: "images/Character_Yunjin.png",
+        icon: "images/Element_Geo.png",
+        header: "char-geo"
+    },
+
+}
+
+// Clear Intital List of Chars
+
+document.querySelector('.display-box').style.display = 'none';
+
+// filter function for elements
+
+(function () {
+
+    const buttons = document.querySelectorAll('.btn')
+    const storeItems = document.querySelectorAll('.char-item')
+
+    buttons.forEach((item) => {
+        item.addEventListener('click', (e) => {
+            e.preventDefault()
+
+            const filter = e.target.dataset.filter
+
+            document.querySelector('.display-box').style.display = 'block';
+
+
+            console.log(e.target.dataset.filter);
+
+
+            storeItems.forEach((item) => {
+                if (filter === 'all') {
+                    item.style.display = 'block'
+                } else {
+                    if (item.classList.contains(filter)) {
+                        item.style.display = 'block'
+                    } else {
+                        item.style.display = 'none'
+                    }
+                }
+            })
+        })
+    })
+
+})();
+
+updateStart();
+
+// function execute only once
+
+let executed = false;
+
+let pyroStart = (function () {
+    return function () {
+        if (!executed) {
+            executed = true;
+            console.log("1");
+
+        }
+    };
+})();
+
+
+
+// Update Data
+
+let pos = 1;
+
+function updateStart() {
+    document.querySelectorAll('.counter li').forEach((li) => {
+        li.addEventListener('click', () => {
+
+            update(li.id, pos);
+            pos = pos + 1;
+        });
+
+    });
+}
+
+// Update Data
+function update(identify, num) {
+
+    if (num == 1) {
+        num = 'one';
+    }
+    else if (num == 2) {
+        num = 'two';
+    }
+    else if (num == 3) {
+        num = 'three';
+    }
+
+    else if (num == 4) {
+        num = 'four';
+    }
+
+    for (let props in database) {
+
+        if (identify == props) {
+            let charImg = document.querySelector(`.char-${num} .character-image`);
+            charImg.src = database[props].image;
+
+            let charName = document.querySelector(`.char-${num} h2`);
+            charName.textContent = database[props].charName;
+            charName.classList.add(database[props].header);
+
+            let iconImg = document.querySelector(`.char-${num} .icon`)
+            iconImg.src = database[props].icon;
+        }
+
+    }
+}
+
+// Resets the DOM to intial state after click
+
+function ResetAll() {
+    window.location.reload();
+    executed = false;
+    pos = 1;
+}
+
+// dom-to-image
+
+document.getElementById("download").addEventListener("click", function () {
+    domtoimage.toJpeg(document.getElementById('screenshot'), { quality: 1 })
+        .then(function (dataUrl) {
+            var link = document.createElement('a');
+            link.download = 'Genshin-Team-Card.jpeg';
+            link.href = dataUrl;
+            link.click();
+        });
+});
+
